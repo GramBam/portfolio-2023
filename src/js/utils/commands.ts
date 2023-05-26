c9ad2685651efae474ec32ba4883de7ed9de59c3
Graham <graham.moss@hotmail.com>
2023-05-25 22:39:40 -0400

    add contact command

--- src/js/utils/commands.ts
+++ src/js/utils/commands.ts
@@ -4,6 +4,7 @@ export const commands = [
   { input: "all", output: blocks.all },
   { input: "about", output: blocks.about },
   { input: "social", output: blocks.social },
+  { input: "contact", output: blocks.social },
   { input: "clear", output: null },
   { input: "stack", output: blocks.stack },
   { input: "resume", output: blocks.resume },
